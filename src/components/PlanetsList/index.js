5830bd3d17eca2a0c5e655894f2383709db45f76
Alena Zhmurko <alenazhmurko@gmail.com>
2020-11-02 08:17:11 +0200

    cleanup planetsList component

--- src/components/PlanetsList/index.js
+++ src/components/PlanetsList/index.js
@@ -40,37 +40,6 @@ const PlanetsList = () => {
       </Container>
       <Container className={style.planetsListFooter}>
         <PlanetsListNav pathname="/planets" currentPage={currentPage} listMeta={listMeta} />
-        {/* <Button icon>
-          <Link to={{pathname: "/planets", search: `?page=${Number(currentPage) - 1}`}} >
-            <Icon name='long arrow alternate left' />
-          </Link>
-        </Button>
-        <span>
-          {currentPage * 10}/{listMeta.count} 
-        </span>
-        <Button icon>
-          <Link to={{pathname: "/planets", search: `?page=${Number(currentPage) + 1}`}} >
-            <Icon name='long arrow alternate right' />
-          </Link>
-        </Button> */}
-        
-          {/* <Link
-            to={{
-            pathname: "/planets",
-              search: `?page=${Number(currentPage) - 1}`
-            }}
-          >
-            Prev
-          </Link>
-          <Link
-            to={{
-              pathname: "/planets",
-              search: `?page=${Number(currentPage) + 1}`
-            }}
-            className="paginationBtn"
-          >
-            Next
-          </Link> */}
       </Container>
     </>
   );
